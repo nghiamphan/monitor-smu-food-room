d4415a0d84c98d54fd60f2b8d6683d9dc7dc1bc1
Nghia <19204686+nghiamphan@users.noreply.github.com>
2023-11-19 14:56:13 -0400

    minor

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def input():
     st.write(st.secrets["URL"])
     st.session_state["is_monitored"] = st.toggle("Monitor registration")
     st.session_state["time_interval"] = st.number_input(
-        "Set time interval between each check", value=5, min_value=1, format="%i"
+        "Set time interval between each check (minutes)", value=5, min_value=1, format="%i"
     )
     st.session_state["receiver_email"] = st.text_input(
         "Email to send notification:", placeholder="Leave empty to use default email"
